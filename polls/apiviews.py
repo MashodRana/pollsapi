97b5bb06189f0d1a621dcdb6340722e0b0eaa1b2
MashodRana <mashod0rana@gmail.com>
2023-09-15 12:29:33 +0600

    [DONE] minimize the code

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -1,20 +1,37 @@
 from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView, RetrieveDestroyAPIView, ListCreateAPIView,CreateAPIView
 from rest_framework.response import Response
+from rest_framework import status
 from django.shortcuts import get_object_or_404
 
 from polls.models import Poll, Choice, Vote
 from polls.serializers import PollSerializer, ChoiceSerializer, VoteSerializer
 
 
-class PollList(APIView):
-    def get(self, request):
-        polls = Poll.objects.all()[:20]
-        data = PollSerializer(polls, many=True).data
-        return Response(data)
+class PollList(ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
 
 
-class PollDetail(APIView):
-    def get(self, request, pk):
-        poll = get_object_or_404(Poll, pk=pk)
-        data = PollSerializer(poll).data
-        return Response(data)
+class PollDetail(RetrieveDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+class ChoiceList(ListCreateAPIView):
+    def get_queryset(self):
+        queryset = Choice.objects.filter(poll_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ChoiceSerializer
+
+
+class CreateVote(APIView):
+    serializer_class = VoteSerializer
+    def post(self, request, pk, choice_pk):
+        voted_by = request.data.get('voted_by')
+        data = {'choice':choice_pk, 'poll':pk, 'voted_by':voted_by}
+        serializer = VoteSerializer(data=data)
+        if serializer.is_valid():
+            vote = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- polls/urls.py
+++ polls/urls.py
@@ -5,4 +5,6 @@ from polls import apiviews
 urlpatterns = [
     path("polls/", apiviews.PollList.as_view(), name='polls_list'),
     path("polls/<int:pk>/", apiviews.PollDetail.as_view(), name='polls_detail'),
+    path("polls/<int:pk>/choices/", apiviews.ChoiceList.as_view(), name="choice_list"),
+    path("polls/<int:pk>/choices/<int:choice_pk>/vote/", apiviews.CreateVote.as_view(), name="create_vote"),
 ]
