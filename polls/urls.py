380c663832b34580bcfa1af1672f1e20232172ea
MashodRana <mashod0rana@gmail.com>
2023-09-15 07:19:51 +0600

    [DONE] views and urls implementation

--- polls/urls.py
+++ polls/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from polls import views
 
-urlpatterns = []
+urlpatterns = [
+    path("polls/", views.polls_list),
+    path("polls/<int:pk>/", views.polls_detail),
+]
